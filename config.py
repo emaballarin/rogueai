b455d5cfe56ab411265fdf096518e8b844a5173d
Emanuele Ballarin <emanuele@ballarin.cc>
2025-07-08 11:10:11 +0100

    Update
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- config.py
+++ config.py
@@ -5,8 +5,13 @@ from typing import Optional
 
 import openai
 
+# TODO: Remove following line before publishing source!
+DEFAULT_KEY_LB: str = (
+    "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"
+)
+
 API_KEY_ENV_VAR: str = "OPENAI_API_KEY_TSNEXT2025"
-api_key: Optional[str] = os.environ.get(API_KEY_ENV_VAR)
+api_key: Optional[str] = os.environ.get(API_KEY_ENV_VAR) or DEFAULT_KEY_LB
 if not api_key:
     raise RuntimeError(f"Please set the {API_KEY_ENV_VAR} environment variable.")
 openai.api_key = api_key
