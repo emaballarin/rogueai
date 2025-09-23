ee5b1a60e92da301f433495a20255e82516a3697
Emanuele Ballarin <emanuele@ballarin.cc>
2025-09-23 16:30:43 +0100

    Minor nitpicks
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- scripts/clean_sessions_and_stats.sh
+++ scripts/clean_sessions_and_stats.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Remove __pycache__, .json files in ../.stats/ and ../.sessions/, ignore errors if files or directories do not exist
-rm -rf ./__pycache__ ../__pycache__ ../.stats/*.json ../.sessions/*.json 2>/dev/null
+rm -rf ./__pycache__ ../__pycache__ ../.stats/*.json ../.sessions/*.json ../.audio/* 2>/dev/null
