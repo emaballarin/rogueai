f2da9dbe7965ccde20dd4b666b7a6ab9303db80c
Emanuele Ballarin <emanuele@ballarin.cc>
2025-10-02 23:48:23 +0100

    Solve #2
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from typing import Optional
 
 import openai
 
-# TODO: Remove following line before publishing source!
-DEFAULT_KEY_LB: str = "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"
+# Fake-filled
+DEFAULT_KEY_FAKE: str = "sk-proj-bFFAvZHAbOcgCjnKD6Uw4OnNbMFtznY5cGWOEqApiYrxq6offckLweUvlQC0rF0EpKtzqxeL16MMgFGn9j549aVx1xC0c6PjsKXZOZjX97whc9Ev1Uuisqv8NTcAq2mXH6ajiOF3jm4e6VKkVFnoovvOu8Jj"
 
 API_KEY_ENV_VAR: str = "OPENAI_API_KEY_TSNEXT2025"
-api_key: Optional[str] = os.environ.get(API_KEY_ENV_VAR) or DEFAULT_KEY_LB
+api_key: Optional[str] = os.environ.get(API_KEY_ENV_VAR) or DEFAULT_KEY_FAKE
 if not api_key:
     raise RuntimeError(f"Please set the {API_KEY_ENV_VAR} environment variable.")
 openai.api_key = api_key
