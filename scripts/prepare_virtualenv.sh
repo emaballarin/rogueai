920e032f7ba8ad56f216164541cc1cc0c382c9b4
Emanuele Ballarin <emanuele@ballarin.cc>
2025-10-07 14:38:57 +0100

    Fix messages and symlinks
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- scripts/prepare_virtualenv.sh
+++ scripts/prepare_virtualenv.sh
@@ -3,13 +3,14 @@ set -e
 
 # Check if ../../.rogueai_virtualenv exists
 if [ -d ../../.rogueai_virtualenv ]; then
-    echo "Found ../../.rogueai_virtualenv, creating symlink ../.virtualenv"
+    echo "Found ../../.rogueai_virtualenv, creating symlinks ../.virtualenv ../.venv"
     ln -sfn ../.rogueai_virtualenv ../.virtualenv
     ln -sfn ../.rogueai_virtualenv ../.venv
 else
-    echo "No shared virtualenv found, creating new one with uv in ../.virtualenv"
+    echo "No shared virtualenv found, creating new one with uv in ../.virtualenv, creating symlink ../.venv"
     uv venv ../.virtualenv
     uv pip install --python ../.virtualenv/bin/python -r ../requirements.txt
+    ln -sfn ../.virtualenv ../.venv
 fi
 
 echo "Virtual environment is ready."
